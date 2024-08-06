e62ccbecf9095023bcd995869a5d9b702b08273f
Isaac7app <ik.appiagyei@gmail.com>
2024-08-06 11:47:22 +0200

    fleshed the app  out a bit

---
+++ src/data/todos.ts
@@ -0,0 +1,17 @@
+export const dummyData = [
+    {
+        id: 1,
+        title: "like this👌🏾",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "To it nicely👌🏾",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Say something 👌🏾",
+        completed: false,
+    },
+];
